253809f9d9b5c7a4a418ed5f1a60fcacd77e78a2
jong_hui <42797995+jong-hui@users.noreply.github.com>
2019-04-29 04:07:44 +0900

    test

--- portfolio_asset/fruitsPang/js/Fruit.js
+++ portfolio_asset/fruitsPang/js/Fruit.js
@@ -5,7 +5,7 @@ class Fruit extends BoardItem {
 	constructor (x, y) {
 		let fruitId = rand(0, Config.fruits.length);
 		let id = Date.now()+(idx++);
-		let item = super(`<div class="item" data-id="${id}"> <img src="imgs/${Config.fruitsSrc[fruitId]}" alt="fruit" /> </div>`, x, y);
+		let item = super(`<div class="item" data-id="${id}"> <img src="/portfolio_asset/fruitsPang/imgs/${Config.fruitsSrc[fruitId]}" alt="fruit" /> </div>`, x, y);
 
 		this.x = x;
 		this.y = y;
